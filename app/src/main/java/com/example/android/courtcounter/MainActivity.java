564b0e59e26009d77af550671e0aeddc178021c6
Anand Prabhu <anandprabhu@protonmail.com>
2016-06-24 18:06:43 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Variable to track team scores
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreTeamA = (TextView) findViewById(R.id.team_a_score);
+        scoreTeamA.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreTeamB = (TextView) findViewById(R.id.team_b_score);
+        scoreTeamB.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increase the score for Team A by 1 points.
+     */
+    public void addOneForTeamA(View view) {
+        scoreTeamA += 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team B by 1 points.
+     */
+    public void addOneForTeamB(View view) {
+        scoreTeamB += 1;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    /**
+     * Reset the scores back to 0
+     */
+    public void resetScore(View view) {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+        displayForTeamA(scoreTeamA);
+        displayForTeamB(scoreTeamB);
+    }
+}
